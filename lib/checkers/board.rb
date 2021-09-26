d9753b9a9859276b4b86818423729d5ad26f323b
Jochen Timmemrans <myrmidex@gmail.com>
2021-09-26 12:46:00 +0200

    [main] - Rework to_array and to_s

--- lib/checkers/board.rb
+++ lib/checkers/board.rb
@@ -9,37 +9,48 @@ class Board
   end
 
   def to_array
-    board_array = []
-    last_letter = nil
-    line = []
-    @board.each do |place, value|
-      letter, number = place.split('')
-      letter_int = (letter.gsub(/[a-z]/) { |m| m.ord - 97 }).to_i
-      if letter_int != last_letter
-        unless last_letter.nil?
-          board_array.push(line)
-          line = []
+    board_array = [
+      [nil,nil,nil,nil,nil,nil,nil,nil,],
+      [nil,nil,nil,nil,nil,nil,nil,nil,],
+      [nil,nil,nil,nil,nil,nil,nil,nil,],
+      [nil,nil,nil,nil,nil,nil,nil,nil,],
+      [nil,nil,nil,nil,nil,nil,nil,nil,],
+      [nil,nil,nil,nil,nil,nil,nil,nil,],
+      [nil,nil,nil,nil,nil,nil,nil,nil,],
+      [nil,nil,nil,nil,nil,nil,nil,nil,],
+    ]
+    ("a".."h").each_with_index do |letter, x|
+      (1..8).each_with_index do |number, y|
+        key = letter.to_s + number.to_s
+        if @board.key?(key)
+          unless @board[key].nil?
+            board_array[y][x] = @board[key]
+          end
         end
-        last_letter = letter_int
       end
-      line.push(value)
+
     end
-    board_array.push(line)
+
     board_array
   end
 
   def to_s
-    output = ""
+    output = "\n    A   B   C   D   E   F   G   H\n"
     board_array = self.to_array
     board_array.each_with_index do |line, index1|
       line.each_with_index do |value, index2|
+        content = ""
+        if index2 === 0
+          output += " " + (index1 + 1).to_s + " "
+        end
+
         if value.nil?
-          content = "    "
+          content += "    "
         else
           if (value.color.to_s === "w")
-            content = " " + (value.to_s).cyan + " "
+            content += " " + (value.to_s).cyan + " "
           else
-            content = " " + (value.to_s).black + " "
+            content += " " + (value.to_s).black + " "
           end
         end
 
